8bf47594ef1c6c59a8b5abfe248a74a2fbdf7309
Jinqi Li <42597907+kikijinqili@users.noreply.github.com>
2021-01-26 11:23:05 -0800

    Update addText.js

--- addText.js
+++ addText.js
@@ -1,5 +1,5 @@
 (function(){
-            let addText = $(".even").eq(0).find(".data");
+            let addText = $j(".even").eq(0).find(".data");
             $(document).ready(function () {
                 console.log(addText);
                 addText.text("China, QC in the USA");
