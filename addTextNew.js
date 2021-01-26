9ca02e508ee05256aefe7287a8a35306764f559f
Jinqi Li <42597907+kikijinqili@users.noreply.github.com>
2021-01-26 13:41:16 -0800

    Update addTextNew.js

--- addTextNew.js
+++ addTextNew.js
@@ -1,7 +1,6 @@
 $(function () {
-            let addText = $("tr.even").eq(0).find("td.data");
+            let addText = $("tr:nth-child(2)").find("td.data");
             $(document).ready(function () {
-                console.log($("tr.even").eq(0).get());
                 console.log(addText);
                 addText.text("China, Quality Control in the United States of America");
             })
